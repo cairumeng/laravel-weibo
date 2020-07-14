5b36b3f3c35622b429e23819ef76a53297af1919
cairumeng <cairumeng@gmail.com>
2020-07-14 18:10:23 +0200

    include header and footer to layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="https://learnku.com/laravel/courses" target=_blank>
+        No pain no gain!</a>
+    <div class="float-right">
+        <a href="/about">About</a>
+    </div>
+</footer>
