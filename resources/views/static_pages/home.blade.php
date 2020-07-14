2737f07c380eb0cb7c31f551419ec9a64ac4463c
cairumeng <cairumeng@gmail.com>
2020-07-14 18:37:45 +0200

    init sign up page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -11,7 +11,7 @@
         Everything begins from here
     </p>
     <p>
-        <a class="btn btn-lg btn-success" href="#" role="button">register now</a>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign up now</a>
     </p>
 </div>
 @stop
