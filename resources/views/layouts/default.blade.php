b37d3f0aedae44c0c904aeeefa9b854d781eaf77
cairumeng <cairumeng@gmail.com>
2020-07-14 17:45:42 +0200

    init home style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,12 +11,17 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div>
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+            </ul>
         </div>
     </nav>
-
-    @yield('content')
-    @yield('js')
-</body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>s
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.default')
 
-@section('title', 'Home')
 
 @section('content')
-<h1>Home</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+        what you are reading right now is the home page of Laravel
+    </p>
+    <p>
+        Everything begins from here
+    </p>
+    <p>
+        <a class="btn btn-lg btn-success" href="#" role="button">register now</a>
+    </p>
+</div>
 @stop
