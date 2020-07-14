135c350524aaea9ae95fd667143650fe7eb7cb3d
cairumeng <cairumeng@gmail.com>
2020-07-14 23:31:20 +0200

    finish user sign up

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,8 +20,20 @@ class UsersController extends Controller
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min:6',
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash(
+            'success',
+            'welcome, you will begin your new journey here!'
+        );
+
+        return redirect()->route('users.show', [$user]);
     }
 }
---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,9 @@
+@foreach(['success','danger','info','warning'] as $message)
+@if(session()->has($message))
+<div class="flash-message">
+    <p class="alert alert-{{$message}}">
+        {{session()->get($message)}}
+    </p>
+</div>
+@endif
+@endforeach
