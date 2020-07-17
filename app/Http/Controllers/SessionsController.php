d73290f43d67c4acf6650fa7fb52053a086d0528
cairumeng <cairumeng@gmail.com>
2020-07-17 12:39:21 +0200

    visit authorisation

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', ['only' => ['create']]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +26,8 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', 'welcome back!');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash(
                 'danger',
